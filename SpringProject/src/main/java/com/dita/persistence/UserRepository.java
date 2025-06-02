61e9649c62ba8bf9f5cf893903c725b6ae3b61f7
dita_810 <dita_810@DESKTOP-7A1VTCU>
2025-06-02 16:21:46 +0900

    Merge branch 'main' of https://github.com/HWAJINJJANG/2025_SpringProject.git into main

--- SpringProject/src/main/java/com/dita/persistence/UserRepository.java
+++ SpringProject/src/main/java/com/dita/persistence/UserRepository.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import com.dita.domain.Grade;
 import com.dita.domain.User;
 
 public interface UserRepository extends JpaRepository<User, String> {
