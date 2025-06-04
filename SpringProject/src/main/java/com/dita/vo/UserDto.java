3762377ea6ec1f03f473e1e28637259b8488d7cd
Yun_A <92855181+Dingrrrr@users.noreply.github.com>
2025-06-04 12:49:11 +0900

    사용자 관리 쪽 환자 쪽 탭 제외하고 구현 완료

---
+++ SpringProject/src/main/java/com/dita/vo/UserDto.java
@@ -0,0 +1,16 @@
+package com.dita.vo;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter @Setter
+public class UserDto {
+    private String usersId;
+    private String usersName;
+    private String usersPhone;
+    private String usersEmail;
+    private String usersAddress;
+    private String usersBirth;
+    private String usersGender;
+    private String usersIdcard;
+}
