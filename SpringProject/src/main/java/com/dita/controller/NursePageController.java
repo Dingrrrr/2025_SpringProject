f8d89d5ef8d69038e0eba1c2fa422f0404f1bdbb
Jangton <dhwlsdn030@naver.com>
2025-06-04 18:17:10 +0900

    간호사 로그인 기능 구현 간호사가 아니면 로그인 안되게 해뒀고 비밀번호 해싱알고리즘 적용 시켰습니다.

--- SpringProject/src/main/java/com/dita/controller/NursePageController.java
+++ SpringProject/src/main/java/com/dita/controller/NursePageController.java
@@ -1,16 +1,31 @@
 package com.dita.controller;
 
+import java.security.Principal;
+
+import org.apache.tomcat.util.net.openssl.ciphers.Authentication;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.dita.domain.Grade;
+import com.dita.domain.User;
+import com.dita.persistence.LoginPageRepository;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpSession;
 import lombok.extern.java.Log;
 
 @Controller
 @Log
 @RequestMapping("/nurse/")
 public class NursePageController {
+	
+	private final LoginPageRepository repo;
+
+    public NursePageController(LoginPageRepository repo) {
+        this.repo = repo;
+    }
 	
 	@GetMapping("/NurseChart")
     public String showNurseChartPage(Model model) {
@@ -29,5 +44,23 @@ public class NursePageController {
 		// 필요 시 model에 데이터 추가 가능
         return "nurse/MedicationRecord"; // templates/nurse/MedicationRecord.html 호출
     }
+	
+	@GetMapping("/NurseHome")
+	public String showNurseHome(HttpServletRequest request, Model model) {
+		HttpSession session = request.getSession(false);
+		if (session == null) {
+			return "redirect:/Login/Login";
+		}
+		User loginUser = (User) session.getAttribute("loginUser");
+		if (loginUser == null || !loginUser.getGrade().equals(Grade.간호사)) {
+			return "redirect:/Login/Login";
+		}
+		
+		model.addAttribute("userName", loginUser.getUsersName());
+        model.addAttribute("usersId", loginUser.getUsersId());
+        model.addAttribute("grade", loginUser.getGrade().name());
+		
+		return "nurse/NurseHome";
+	}
 	
 }
---
+++ SpringProject/src/main/java/com/dita/persistence/NotifRepository.java
@@ -0,0 +1,12 @@
+package com.dita.persistence;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.dita.domain.Notif;
+import com.dita.domain.User;
+
+public interface NotifRepository extends JpaRepository<Notif, Integer> {
+	List<Notif> findByUserOrderByCreatedAtDesc(User user);
+}
