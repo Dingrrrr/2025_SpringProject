f748f2a6c9b6deb81e905f9ab9d6b6f18f247f74
Yun_A <92855181+Dingrrrr@users.noreply.github.com>
2025-06-05 14:20:07 +0900

    관리자 페이지 스케줄 추가 기능 구현 완료

--- SpringProject/src/main/java/com/dita/vo/SchedDto.java
+++ SpringProject/src/main/java/com/dita/vo/SchedDto.java
@@ -1,16 +1,24 @@
 package com.dita.vo;
 
 import com.dita.domain.Type;
+import com.fasterxml.jackson.annotation.JsonFormat;
+
 import lombok.Data;
 import java.time.LocalDateTime;
+import java.time.LocalTime;
 import java.time.DayOfWeek;
 import java.util.List;
 
 @Data
 public class SchedDto {
     private int scheduleId;
-    private LocalDateTime startTime;
-    private LocalDateTime endTime;
+    
+    @JsonFormat(pattern = "HH:mm")
+    private LocalTime startTime;
+    
+    @JsonFormat(pattern = "HH:mm")
+    private LocalTime endTime;
+    
     private Type type;
     private List<String> workDays; // 이제 "월", "화", ... 형태의 문자열 리스트
     private String usersId;
