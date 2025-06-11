b1a5625206b2960d3c5e0b6b16df49de306d6be7
dita_806 <dita_806@DESKTOP-S3U0BCA>
2025-06-11 12:28:15 +0900

    환자검색 및 추가메모, 진단검색가능

--- SpringProject/src/main/java/com/dita/persistence/VitalRepository.java
+++ SpringProject/src/main/java/com/dita/persistence/VitalRepository.java
@@ -11,4 +11,6 @@ public interface VitalRepository extends JpaRepository<Vital_sign, Integer> {
 
  // 특정 환자(patient_id)의 Vital 기록 중 가장 최근(recordedAt 기준) 1건 반환
  Optional<Vital_sign> findFirstByPatient_PatientIdOrderByRecordedAtDesc(Integer patientId);
+ 
+ 
 }
